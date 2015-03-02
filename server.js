201a473a3dc8613219d0215336e1aeb005df0777
spautz <spautz@web.de>
2015-03-02 21:02:33 +0000

    first version

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var redis = require("redis");
+var nconf = require('nconf');
+var bodyParser = require('body-parser')
 
-var openConnections = [];
+var openConnections = {};
+var redisClients = {};
 
-app.get('/sse', function(req, res){
-	req.socket.setTimeout(Infinity);
+nconf.argv().env().file({ file: 'settings.json' });
+app.use( bodyParser.json() );
+
+
+app.get('/sse/subscribe', function(req, res){	
+	//req.socket.setTimeout(Infinity);
+	//req.setTimeout(Infinity);
+	//res.setTimeout(Infinity);
+	
 	res.writeHead(200, {
         'Content-Type': 'text/event-stream',
         'Cache-Control': 'no-cache',
@@ -14,30 +25,101 @@ app.get('/sse', function(req, res){
     });
 	res.write('\n');
 	
-	console.log('Channel "'+ req.param("channel") +'" opened from '+req.ip)
+	res.write("retry: 1 \n\n");
 	
-	openConnections.push(res);
+	console.log('Channel "'+ req.param("channel") +'" opened from '+req.ip)
+	console.log('Webtoken: '+req.param("token"));
+	
+	if(openConnections[req.param("channel")]===undefined){
+		openConnections[req.param("channel")] = [res];
+	} else {
+		openConnections[req.param("channel")].push(res);
+	}
+	
+    if (redisClients[req.param("channel")]===undefined) { 
+    	redisClient = redis.createClient(nconf.get('redis_port'),nconf.get('redis_ip'),{auth_pass: nconf.get('redis_auth')})
+		redisClient.subscribe(req.param("channel"));
+		redisClient.on("message", function (channel, message) {
+	      console.log("Redis Channel " + channel + ": " + message);
+	      for(var i=0; i<(openConnections[channel]).length; i++){
+	    	  timestamp = new Date();
+	    	  openConnections[channel][i].write('id: ' + timestamp.getMilliseconds() + '\n');
+	    	  openConnections[channel][i].write('data:' + message +   '\n\n');
+	      }
+		});
+		redisClients[req.param("channel")]=redisClient;
+    }
 	
 	// When the request is closed, e.g. the browser window
     // is closed. We search through the open connections
     // array and remove this connection.
     req.on("close", function() {
         var toRemove;
-        for (var j =0 ; j < openConnections.length ; j++) {
-            if (openConnections[j] == res) {
-                toRemove =j;
-                break;
+        for (channel in openConnections) {
+            if (channel === req.param('channel')) {
+                for(var j=0 ; j< openConnections[channel].length; j++){
+                	if(openConnections[channel][j] == res){
+                		toRemove=j;
+                        break;
+                	}
+                }
+            }
+            if(toRemove!=undefined){
+            	connections = openConnections[channel];
+            	connections.splice(toRemove,1);
+            	console.log("Channel '"+channel+"' closed");
+            	if(connections.length===0){
+                	delete openConnections[channel];
+                	redisClients[channel].unsubscribe();
+                	redisClients[channel].end();
+                	delete redisClients[channel];
+                } else {
+                	openConnections[channel]=connections;
+                }
+            	break;
             }
         }
-        openConnections.splice(j,1);
-        console.log(openConnections.length);
     });
 });
+
+app.post('/sse/publish', function(req, res){
+	channel = req.param("channel");
+	event = req.param('event');		
+	if(openConnections[channel]!=undefined){
+		for(var i=0; i<openConnections[channel].length; i++){
+			timestamp = new Date();
+			var str = "";
+			if(event!=undefined){
+				openConnections[channel][i].write('event: ' +event+"\n");
+			}
+			openConnections[channel][i].write('id: ' + timestamp.getMilliseconds() + '\n');
+			openConnections[channel][i].write('data:' + JSON.stringify(req.body) +   '\n\n');
+		}
+	}
+	res.sendStatus(200);
+});
+
+setInterval(function() {
+    console.log("Send connection refresh...");
+    for(channel in openConnections) {
+    	openConnections[channel].forEach(function(resp){
+    		timestamp = new Date();
+        	resp.write('event: refresh\n')
+            resp.write('id: ' + timestamp.getMilliseconds() + '\n');
+            resp.write('data: \n\n');
+    	});
+    }
+}, nconf.get('refresh_timeout'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-http.listen(9876, function(){
-  console.log('listening on *:9876');
+http.listen(nconf.get('port'), nconf.get('host'), function(){
+  console.log('listening on '+nconf.get('host')+':'+nconf.get('port'));
+  
+  
+  
+  
 });
+
